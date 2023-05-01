89aba494d4e2aaf078efc6def8f2a6deaf22f1a4
rohit <rohit.rew@gmail.com>
2023-05-01 16:22:51 +0530

    deleted unused files

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,34 +2,45 @@ import Header from "@/components/Header";
 import DetailView from "@/components/detailView";
 import NotificationView from "@/components/notificationView";
 import ReportView from "@/components/reportView";
-import React from "react"
+import React from "react";
 
 export default function Home() {
+  const [notificationIsOpen, setNotification] = React.useState(false);
+  const [isDetailView, setDetailView] = React.useState(false);
+  const [isReportView, setReportView] = React.useState(false);
 
-  const [notificationIsOpen , setNotification] = React.useState(false)
-  const [isDetailView , setDetailView] = React.useState(false)
-  const [isReportView , setReportView] = React.useState(false)
-
-
-  const openNotification = ()=>{
-    setNotification(true)
-  }
+  const openNotification = () => {
+    setNotification(true);
+  };
 
   return (
     <main className="h-screen select-none">
       <Header />
 
-
-
       <div className="w-screen  flex items-center justify-center h-90per relative">
-        {notificationIsOpen || <button onClick={openNotification} className="bg-green-700 rounded-full px-5 py-3 text-white ">1 new notification</button>}
+        {notificationIsOpen || (
+          <button
+            onClick={openNotification}
+            className="bg-green-700 rounded-full px-5 py-3 text-white "
+          >
+            1 new notification
+          </button>
+        )}
 
-
-        {notificationIsOpen && <NotificationView closeNotification={setNotification} openDetailView={setDetailView}/>}
-        {isDetailView && <DetailView closeDetailView={setDetailView} openReportView={setReportView}/>}
-        {isReportView && <ReportView closeReportView={setReportView}/>}
+        {notificationIsOpen && (
+          <NotificationView
+            closeNotification={setNotification}
+            openDetailView={setDetailView}
+          />
+        )}
+        {isDetailView && (
+          <DetailView
+            closeDetailView={setDetailView}
+            openReportView={setReportView}
+          />
+        )}
+        {isReportView && <ReportView closeReportView={setReportView} />}
       </div>
     </main>
-
-  )
+  );
 }
